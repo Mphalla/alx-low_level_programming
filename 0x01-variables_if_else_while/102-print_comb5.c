a3df6f547cb15bcbfe5229f6e5f6db212e0fbca2
Mphalla <letlabza@.com>
2022-07-03 03:04:36 -0700

    102-print_comb5.c

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -7,20 +7,26 @@
  */
 int main(void)
 {
-int ten;
-int one;
-for (ten = 0; ten <= 9; ten++)
+int first_num;
+int second_num;
+for (first_num = 0; first_num < 100; first_num++)
 {
-for (one = 0; one <= 9; one)
+for (second_num = first_num + 1; second_num < 100; second_num++)
 {
-putchar(ten + '0');
-putchar(one + '0');
-if (!(ten == 9 && one == 9))
+putchar(first_num / 10 + '0');
+putchar(first_num % 10 + '0');
+putchar(' ');
+putchar(second_num / 10 + '0');
+putchar(second_num % 10 + '0');
+if (first_num == 98 && second_num == 99)
 {
+break;
+}
+
 putchar(',');
 putchar(' ');
 }
 }
-}putchar('\n');
+putchar('\n');
 return (0);
 }
