7cd6bb079798ced64be671199fc0dbde703da818
Mphalla <letlabza@.com>
2022-07-07 17:52:45 -0700

    3-print_numbers.c

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -3,7 +3,6 @@
 /**
  * print_numbers -  prints the numbers, from 0 to 9,
  * followed by a new line
- * Return: Always 0
  */
 void print_numbers(void)
 {
@@ -12,6 +11,6 @@ void print_numbers(void)
 	for (c = '0'; c <= '9'; c++)
 	{
 		_putchar(c);
-		_putchar('\n');
 	}
+	_putchar('\n');
 }
