08ecb2a8105ba5c3c830f3c686035af1c484b7fa
Mphalla <letlabza@.com>
2022-07-11 19:34:01 -0700

    101-keygen.c

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,18 @@
+#include "main."
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - entry point
+ * Return: Always 0
+ */
+int main(void)
+{
+	int number;
+
+	srand(time(0));
+	number = rand();
+	printf("%i\n", number);
+	return (0);
+}
